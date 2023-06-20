7aef21b9ba736221b5aa9dcdbc72945ef2aca497
Donny <112881062+D0N09@users.noreply.github.com>
2023-06-20 10:25:21 +0200

    Add files via upload

--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -12,13 +12,39 @@ console.log("yo, deze script werkt");
 // in de functie waarin ze zijn gedeclareerd.
 
 // Variabele gemaakt voor list en favolist.
-var list = document.querySelector('main section ul');
-var favolist = document.querySelector('main section:nth-of-type(2) ul')
+var list = document.querySelector('main section:nth-of-type(1) ul');
+var cart = document.querySelector('main section:nth-of-type(2) ul')
 
 // Variabele gemaakt voor de filter opties (all, survival en royale).
 var optionAll = document.querySelector("#filter-all");
 var optionsurvival = document.querySelector("#filter-survival");
 var optionroyale = document.querySelector("#filter-royale");
+
+
+// CAROUSEL
+
+
+// Variabele gemaakt voor de carousel.
+var options = {
+    // De richting van de carousel (rechts/links).
+    direction: 'horizontal',
+    // Hoelang de transitie duurt.
+    speed: 1000,
+  
+    // Navigatie (arrows).
+    navigation: {
+    // Roep de next button aan
+    nextEl: '.swiper-button-next',
+    // Roep de prev button aan
+    prevEl: '.swiper-button-prev'
+}};
+  
+// Navigeren van de carousel
+const swiper = new Swiper('.swiper', options);
+
+
+// FILTER
+
 
 // AddeventListener toegevoegd die zorgt ervoor dat de
 // new Sortable list en favolist aangeroepen worden.
@@ -26,31 +52,6 @@ optionAll.addEventListener("change", filterList);
 optionsurvival.addEventListener("change", filterList);
 optionroyale.addEventListener("change", filterList);
 
-// Sorteeer optie "list" aangemaakt.
-new Sortable(list, {
-    group: {
-        // Je kan elmenten uit deze lijst delen.
-        name: 'shared',
-        // Het element word gekopieerd naar de andere lijst (gedupliceerd).
-        pull: 'clone',
-        // Er kan niks in deze lijst toegevoegd worden.
-        put: false
-    },
-    // De tijd hoe lang de animatie duurt om verplaatst te worden.
-    // Hoe hoger de tijd hoe langzamer de animatie gaat.
-    animation: 150
-});
-
-// Sorteeer optie "favolist" aangemaakt.
-new Sortable(favolist, {
-    group: {
-        // Je kan elmenten uit deze lijst delen.
-        name: 'shared',
-    },
-    // De tijd hoe lang de animatie duurt om verplaatst te worden.
-    // Hoe hoger de tijd hoe langzamer de animatie gaat.
-    animation: 150
-});
 
 // Functie aangemaakt "filterList"
 function filterList(event){
@@ -67,3 +68,33 @@ function filterList(event){
     // Aan de class van "deLijst" word de let (nieuweFilter) toegevoegd.
     deLijst.classList.add(nieuweFilter);
 }
+
+
+// EERSTE LIST
+
+
+// Sorteeer optie "list" aangemaakt.
+new Sortable(list, {
+    group: {
+        // Je kan elmenten uit deze lijst delen.
+        name: 'shared',
+    },
+    // De tijd hoe lang de animatie duurt om verplaatst te worden.
+    // Hoe hoger de tijd hoe langzamer de animatie gaat.
+    animation: 150
+});
+
+
+// TWEEDE LIST
+
+
+// Sorteeer optie "favolist" aangemaakt.
+new Sortable(cart, {
+    group: {
+        // Je kan elmenten uit deze lijst delen.
+        name: 'shared',
+    },
+    // De tijd hoe lang de animatie duurt om verplaatst te worden.
+    // Hoe hoger de tijd hoe langzamer de animatie gaat.
+    animation: 150
+});
